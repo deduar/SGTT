63b469387b7d2ffcb51367af47b77f419670765f
deduar <deduar@gmail.com>
2016-09-16 15:22:23 -0400

    empleado with supervisor

--- src/deduar/S3SandBoxBundle/Form/EmpleadoType.php
+++ src/deduar/S3SandBoxBundle/Form/EmpleadoType.php
@@ -18,6 +18,7 @@ class EmpleadoType extends AbstractType
             ->add('login')
             ->add('ficha')
             ->add('idpersona')
+            ->add('idsupervisor')
         ;
     }
     
