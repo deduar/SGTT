a850e9670da56bace5585cc8c684cd74e09b781d
deduar <deduar@gmail.com>
2016-10-24 23:07:50 -0400

    reset

--- src/deduar/S3SandBoxBundle/Controller/ExcepcionController.php
+++ src/deduar/S3SandBoxBundle/Controller/ExcepcionController.php
@@ -93,7 +93,6 @@ class ExcepcionController extends Controller
                 $criteria = 'remunerada';
                 $dir = 'ASC';
                 break;
-            
             default:
                 $criteria = 'id';
                 $dir = 'DESC';
