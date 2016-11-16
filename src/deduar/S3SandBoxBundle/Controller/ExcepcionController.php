6af4f63833a1cfe7e17c533c7b8dc3581d8dbed8
deduar <deduar@gmail.com>
2016-11-15 21:42:58 -0400

    minor bugs

--- src/deduar/S3SandBoxBundle/Controller/ExcepcionController.php
+++ src/deduar/S3SandBoxBundle/Controller/ExcepcionController.php
@@ -68,7 +68,7 @@ class ExcepcionController extends Controller
                 ->findOneBy(array('id'=>$excepcion->getIdempleado()));
 
         
-        if (sizeof($em_j)) {
+        if (sizeof($em_j) > 0) {
             print_r($em_j->getIdsupervisor()->getId()); echo"<br>";
             $p_j = $em->getRepository('S3SandBoxBundle:Persona')
                     ->findOneBy(array('id'=>$em_j->getIdsupervisor()->getId()));
